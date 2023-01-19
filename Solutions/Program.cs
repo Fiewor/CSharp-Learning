3f2ced1fe69e4c80662b3aaab88836d6bec77976
Fiewor <johnfiewor@gmail.com>
2023-01-19 13:49:02 +0100

    add code for debugging practice

--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using System.Net.NetworkInformation;
+
+public class Program
 {
 
     public static void Exercise1()
@@ -382,6 +384,40 @@
     }
     private static void Main(string[] args)
     {
-        ExerciseD5();
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var smallests = GetSmallests(numbers, 3);
+
+        foreach (var number in smallests)
+            Console.WriteLine(number); 
+    }
+
+    public static List<int> GetSmallests(List<int> list, int count)
+    {
+        if (list == null)
+            throw new ArgumentNullException("list");
+        if (count > list.Count || count <= 0)
+            throw new ArgumentOutOfRangeException("count", "Count should be between 1 and the number of elements in the list.");
+
+        var buffer = new List<int>(list);
+        var smallests = new List<int>();
+
+        while(smallests.Count < count)
+        {
+            var min = GetSmallest(buffer);
+            smallests.Add(min);
+            buffer.Remove(min);
+        }
+        return smallests;
+    }
+
+    public static int GetSmallest(List<int> list)
+    {
+        var min = list[0];
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] < min)
+                min = list[i];
+        }
+        return min;
     }
 }
