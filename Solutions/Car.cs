055ac63b862fc9893cea6b8b82c1058a89d6ea17
Fiewor <johnfiewor@gmail.com>
2023-03-15 14:17:55 +0100

    cde snippets to explain constructors, inheritance and the base keyword

---
+++ Solutions/Car.cs
@@ -0,0 +1,16 @@
+﻿public partial class Program
+{
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized");
+        }
+        public Car(string registrationNumber)
+            : base(registrationNumber) // a way to access the base class
+        {
+            // initialise fields specific to the Car class
+            Console.WriteLine("Car is being initialized, {0}", registrationNumber);
+        }
+    }
+}
---
+++ Solutions/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿public partial class Program
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized, {0}", _registrationNumber);
+        }
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+        }
+    }
+}
